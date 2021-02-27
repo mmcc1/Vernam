8d3afaa0a5a1303ae30bb5679ac628e49f1925ea
Marx Bitware <info@marxbitware.co.uk>
2021-02-27 15:47:00 +0700

    Initial commit

---
+++ LibVernam/Vernam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace LibVernam
+{
+    public static class Vernam
+    {
+        public static byte[] Encrypt(byte[] plainText, byte[] key)
+        {
+            if (plainText.Length != key.Length)
+                return null;
+
+            BitArray dataBits = new BitArray(plainText);
+            BitArray keyBits = new BitArray(key);
+            BitArray cipherBits = new BitArray(plainText.Length * 8);
+
+            for(int i = 0; i < dataBits.Length; i++)
+            {
+                cipherBits[i] = dataBits[i] ^ keyBits[i];
+            }
+
+            return Helpers.BitArrayToByteArray(cipherBits);
+        }
+
+        public static byte[] Decrypt(byte[] ciphertext, byte[] key)
+        {
+            if (ciphertext.Length != key.Length)
+                return null;
+
+            BitArray dataBits = new BitArray(ciphertext);
+            BitArray keyBits = new BitArray(key);
+            BitArray plainTextBits = new BitArray(ciphertext.Length * 8);
+
+            for (int i = 0; i < dataBits.Length; i++)
+            {
+                plainTextBits[i] = dataBits[i] ^ keyBits[i];
+            }
+
+            return Helpers.BitArrayToByteArray(plainTextBits);
+        }
+    }
+}
---
+++ VernamCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using LibVernam;
+using System;
+using System.Text;
+
+namespace VernamCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToBeEncrypted = "This is a string of text.";
+            byte[] bytesToBeEncrypted = Encoding.ASCII.GetBytes(textToBeEncrypted);
+            byte[] key = Helpers.GenerateRandomBytes(bytesToBeEncrypted.Length);
+
+            byte[] cipherText = Vernam.Encrypt(bytesToBeEncrypted, key);
+            byte[] bytesDeciphered = Vernam.Encrypt(bytesToBeEncrypted, key);
+
+            string textDeciphered = Encoding.ASCII.GetString(Vernam.Decrypt(cipherText, key));
+            
+            Console.WriteLine(textDeciphered);
+            
+            Console.ReadLine();
+        }
+
+        
+    }
+}
