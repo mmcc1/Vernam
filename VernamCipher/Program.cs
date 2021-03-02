41692505d5b6d56bd537a5260f152ad608a767c1
Marx Bitware <info@marxbitware.co.uk>
2021-03-02 11:14:28 +0700

    Add VernamHash

--- VernamCipher/Program.cs
+++ VernamCipher/Program.cs
@@ -8,6 +8,8 @@ namespace VernamCipher
     {
         static void Main(string[] args)
         {
+            /*
+            //Use Vernam Cipher
             string textToBeEncrypted = "This is a string of text.";
             byte[] bytesToBeEncrypted = Encoding.ASCII.GetBytes(textToBeEncrypted);
             byte[] key = Helpers.GenerateRandomBytes(bytesToBeEncrypted.Length);
@@ -16,7 +18,22 @@ namespace VernamCipher
             string textDeciphered = Encoding.ASCII.GetString(Vernam.Decrypt(cipherText, key));
             
             Console.WriteLine(textDeciphered);
-            
+            */
+
+
+            //Use VernamHash
+            string textToEncrypt = "This is a test.";
+            byte[] bytesToEncrypt = Encoding.ASCII.GetBytes(textToEncrypt);
+            byte[] key = Helpers.GenerateRandomBytes(64);  //Key is 64 bytes (512 bits) in length
+            int byteSelection = 4;  //Select a byte from hash, valid values for SHA512 is 0-64.
+
+            byte[] cipherText = VernamHash.Encrypt(key, bytesToEncrypt, byteSelection);
+            byte[] decipheredText = VernamHash.Encrypt(key, cipherText, byteSelection);
+
+            string textDeciphered = Encoding.ASCII.GetString(decipheredText);
+
+            Console.WriteLine(textDeciphered);
+
             Console.ReadLine();
         }
     }
